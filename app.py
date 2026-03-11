df44468deb8ef3980a631ee05004dfb6a324eafb
giraso2 <girasoesperrance@gmail.com>
2026-03-11 04:52:39 -0700

    new update on erinde

--- app.py
+++ app.py
@@ -1,50 +1,42 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, session, redirect
 
 app = Flask(__name__)
 app.secret_key="secret"
 
 
-@app.route("/")
-def home():
-    return render_template("login.html")
-
-
-@app.route("/login",methods=["POST"])
-def login():
-
-    email=request.form["email"]
-
-    session["user"]=email
-
-    return redirect("/user")
-
-
-@app.route("/signup",methods=["POST"])
-def signup():
-
-    name=request.form["name"]
-    email=request.form["email"]
-
-    session["user"]=email
-
-    return redirect("/user")
-
-
-@app.route("/user")
+@app.route("/dashboard")
 def dashboard():
-
-    if "user" in session:
-        return render_template("user.html", user=session["user"])
-
-    return redirect("/")
+    return render_template("dashboard.html", user=session.get("user"))
 
 
-@app.route("/logout")
-def logout():
+@app.route("/location")
+def location():
+    return render_template("location.html")
 
-    session.pop("user",None)
 
-    return redirect("/")
+@app.route("/hospitals")
+def hospitals():
+    return render_template("hospitals.html")
+
+
+@app.route("/doctors")
+def doctors():
+    return render_template("doctors.html")
+
+
+@app.route("/book")
+def book():
+    return render_template("book.html")
+
+
+@app.route("/appointments")
+def appointments():
+    return render_template("appointments.html")
+
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
 
 
 app.run(debug=True)
